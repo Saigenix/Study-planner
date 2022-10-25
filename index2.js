be5bd1eb7a99b65167cde184fa40df2839208d61
saigenix <72882933+Saigenix@users.noreply.github.com>
2022-10-25 22:27:12 +0530

    update

--- index2.js
+++ index2.js
@@ -24,7 +24,10 @@ const showNotes = (e) => {
     notesElm.innerHTML = `Nothing to show!`;
   }
 }
-showNotes();
+window.addEventListener('load', (event) => {
+  console.log('page is fully loaded');
+ showNotes();
+});
 // to search function
 
 let search = document.getElementById('searchTxt');
